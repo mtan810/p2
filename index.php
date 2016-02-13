717b9b0f5707488a5522e957124f5754232f7a31
Mason Tan <mtan810@gmail.com>
2016-02-13 18:50:05 -0500

    Add form

--- index.php
+++ index.php
@@ -4,6 +4,26 @@
     <title>Mason Tan P2 - xkcd Password Generator</title>
 </head>
 <body>
-    <h1>xkcd Password Generator</h1>    
+
+    <h1>xkcd Password Generator</h1>
+    
+    <form action='index.php' method='GET'>
+    
+        <label for='word_count'>How many words do you want in your password?</label>
+        <input type='text' name=word_count>
+        <br>
+        
+        <input type='checkbox' name='number_check'>
+        <label for='number_check'>Check if you want to add a number</label>
+        <br>
+        
+        <input type='checkbox' name='symbol_check'>
+        <label for='symbol_check'>Check if you want to add a symbol</label>
+        <br>
+        
+        <input type='submit' value='Generate Password!'>
+        
+    </form>
+    
 </body>
 </html>
